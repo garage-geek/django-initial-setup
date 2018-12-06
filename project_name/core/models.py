4487a663f91a3c7f82729d7c86a48ae414d43fc9
Adriano Margarin <adriano.margarin@gmail.com>
2018-12-06 14:13:19 -0200

    Settings

--- project_name/core/models.py
+++ project_name/core/models.py
@@ -1,7 +1,6 @@
 from uuid import uuid4
 
 from django.db import models
-from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -10,5 +9,6 @@ class AbstractBaseModel(models.Model):
     class Meta:
         abstract = True
 
+    uuid = models.UUIDField(verbose_name=_('UUID'), default=uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(verbose_name=_('Created at'), auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name=_('Updated at'), auto_now=True)
